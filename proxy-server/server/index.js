acfbd92116692de9149918e02a2a8188f11753c1
zjayers <zayers.dev@gmail.com>
2020-06-29 15:42:03 -0700

    updates index.html and stlyes

--- proxy-server/server/index.js
+++ proxy-server/server/index.js
@@ -4,12 +4,12 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
 
-app.get('*', (req, res) => {
+app.get('/:1', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
+
 app.listen(4000, () => {
   console.log('Proxy Server is listening on port 4000');
 });
