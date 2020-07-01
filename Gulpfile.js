b05f1f1737ac23167da5c86bee67b70644d87f1f
zjayers <zayers.dev@gmail.com>
2020-07-01 13:00:26 -0700

    bundles and css hosted on aws

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs");
 const s3 = new AWS.S3();
 
-const upload = function (filePath, newName) {
+const upload = function (filePath, newName, type) {
   const fileName = path.join(__dirname, filePath);
   const fileContent = fs.readFileSync(fileName);
 
@@ -12,7 +12,10 @@ const upload = function (filePath, newName) {
     Bucket: "fec-zayers-proxy",
     Key: newName, // File name you want to save as in S3
     Body: fileContent,
+    ACL: "public-read",
+    ContentType: type,
   };
+
   // Uploading files to the bucket
   s3.upload(params, function (err, data) {
     if (err) {
@@ -23,13 +26,17 @@ const upload = function (filePath, newName) {
 };
 
 gulp.task("publish", function (done) {
-  upload("./campaign/client/dist/bundle.js", "campaign.js");
-  upload("./navbar/client/dist/bundle.js", "navbar.js");
-  upload("./pledge-rewards/client/dist/bundle.js", "rewards.js");
-  upload("./pledge-rewards/client/dist/main.css", "rewards.css");
-  upload("./tab-bar/client/dist/bundle.js", "tabbar.js");
-  upload("./VA-Service/dist/bundle.js", "banner.js");
-  upload("./WWL---Service/dist/main.js", "updates.js");
-  upload("./proxy-server/public/style.css", "proxy.css");
+  upload("./campaign/client/dist/bundle.js", "campaign.js", "text/javascript");
+  upload("./navbar/client/dist/bundle.js", "navbar.js", "text/javascript");
+  upload(
+    "./pledge-rewards/client/dist/bundle.js",
+    "rewards.js",
+    "text/javascript"
+  );
+  upload("./pledge-rewards/client/dist/main.css", "rewards.css", "text/css");
+  upload("./tab-bar/client/dist/bundle.js", "tabbar.js", "text/javascript");
+  upload("./VA-Service/dist/bundle.js", "banner.js", "text/javascript");
+  upload("./WWL---Service/dist/main.js", "updates.js", "text/javascript");
+  upload("./proxy-server/public/style.css", "proxy.css", "text/css");
   done();
 });
