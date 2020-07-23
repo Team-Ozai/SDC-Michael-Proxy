c5d6bc056296d876a2cb23d1c5ba49cbd0f00cf8
Michael Taylor <cardzfan1207@gmail.com>
2020-07-23 19:30:44 -0400

    Updated with deployed service load balancer ip

--- proxy-server/server/index.js
+++ proxy-server/server/index.js
@@ -1,22 +1,23 @@
+const newrelic = require('newrelic');
 const express = require('express');
 const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const rewardsServiceRoute =
-  'http://ec2-3-133-92-215.us-east-2.compute.amazonaws.com:3005';
+// const rewardsServiceRoute =
+//   'http://ec2-3-133-92-215.us-east-2.compute.amazonaws.com:3005';
 
 const proxyRouter = {
-  '/api/banner': 'http://localhost:3002',
-  'api/video': 'http://localhost:3002',
-  'api/update': 'http://localhost:3001',
-  'api/comment': 'http://localhost:3001',
-  'api/story': 'http://localhost:3003',
-  'api/RisksAndChallenges': 'http://localhost:3003',
-  'api/EnvironmentalCommitments': 'http://localhost:3003',
-  'api/projects': rewardsServiceRoute,
-  'api/rewards': rewardsServiceRoute,
+  // '/api/banner': 'http://localhost:3002',
+  // 'api/video': 'http://localhost:3002',
+  // 'api/update': 'http://localhost:3001',
+  // 'api/comment': 'http://localhost:3001',
+  // 'api/story': 'http://localhost:3003',
+  // 'api/RisksAndChallenges': 'http://localhost:3003',
+  // 'api/EnvironmentalCommitments': 'http://localhost:3003',
+  'api/projects': 'http://sdc-lb1-582980772.us-east-2.elb.amazonaws.com:3005/',
+  'api/rewards': 'http://sdc-lb1-582980772.us-east-2.elb.amazonaws.com:3005/'
 };
 
 app.use(express.static(path.join(__dirname, '../dist')));
