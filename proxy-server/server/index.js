2787cb3db7722532ab3aabce1b83311ff93bd669
Michael Taylor <cardzfan1207@gmail.com>
2020-07-24 19:03:08 -0400

    Updated with all services attached

--- proxy-server/server/index.js
+++ proxy-server/server/index.js
@@ -9,13 +9,13 @@ const app = express();
 //   'http://ec2-3-133-92-215.us-east-2.compute.amazonaws.com:3005';
 
 const proxyRouter = {
-  // '/api/banner': 'http://localhost:3002',
-  // 'api/video': 'http://localhost:3002',
-  // 'api/update': 'http://localhost:3001',
-  // 'api/comment': 'http://localhost:3001',
-  // 'api/story': 'http://localhost:3003',
-  // 'api/RisksAndChallenges': 'http://localhost:3003',
-  // 'api/EnvironmentalCommitments': 'http://localhost:3003',
+  '/api/banner': 'http://18.215.78.52:3002',
+  'api/video': 'http://18.215.78.52:3002',
+  'api/update': 'http://18.219.117.44:3001',
+  'api/comment': 'http://18.219.117.44:3001',
+  'api/story': 'http://15.164.34.94:3003',
+  'api/RisksAndChallenges': 'http://15.164.34.94:3003',
+  'api/EnvironmentalCommitments': 'http://15.164.34.94:3003',
   'api/projects': 'http://sdc-lb1-582980772.us-east-2.elb.amazonaws.com:3005/',
   'api/rewards': 'http://sdc-lb1-582980772.us-east-2.elb.amazonaws.com:3005/'
 };
